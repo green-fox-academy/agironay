c1c283cf3aa603fae979f1a947bd29dddc232b75
Agi Ronay <agi.ronay93@gmail.com>
2019-06-27 14:09:30 +0200

    commit i think exc.8

---
+++ greenfox/agironay/week-01/day-4/Exercises/swap.ts
@@ -0,0 +1,10 @@
+// Swap the values of these variables
+let a: number = 123;
+let b: number = 526;
+let c: number = 0;
+
+c = a;
+a = b;
+b = c;
+
+console.log('Now this is the new a: ',a, 'And this is the new b: ', b);
