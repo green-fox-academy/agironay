b181ccac1775682cc0610f191d6e6f04323ea84c
Agi Ronay <agi.ronay93@gmail.com>
2019-06-27 14:44:11 +0200

    commit exc.10

---
+++ greenfox/agironay/week-01/day-4/Exercises/define-basic-info.ts
@@ -0,0 +1,13 @@
+// Define several things as a variable, then print their values
+// Your name as a string
+// Your age as a number
+// Your height in meters as a number
+// Whether you are married or not as a boolean
+
+let myName = 'Agi';
+let myAge = 25;
+let myHeight = 1.75;
+let MarriedOrNot = false;
+
+
+console.log('My name is ',myName, 'I am ',myAge,'years old.','I\'m kinda tall',myHeight,'m last time I checked','and am I married?',MarriedOrNot);
