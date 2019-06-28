c49e03130fb162bd6ade8673f37c50ef255725ad
Agi Ronay <agi.ronay93@gmail.com>
2019-06-28 10:38:35 +0200

    thirdcomm

---
+++ greenfox/agironay/week-01/day-4/Exercises/print-even.ts
@@ -0,0 +1,11 @@
+// Create a program that prints all the even numbers between 0 and 500
+
+
+for (let i: number = 1; i < 500; i++){
+if (i%2 == 0) {
+    console.log(i);
+  }}
+
+for (let i: number = 2; i < 500; i+=2){
+    console.log(i);
+    }
