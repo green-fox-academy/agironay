69a0d77ae845217a1dfa933375474f9dd0240590
Agi Ronay <agi.ronay93@gmail.com>
2019-06-28 10:57:16 +0200

    4th comm maybe

---
+++ greenfox/agironay/week-01/day-4/Exercises/multiplication-table.ts
@@ -0,0 +1,8 @@
+// Create a program that
+// prints the multiplication table with number
+
+let number: number = 20;
+
+for (let i: number = 1; i < 11; i++) {
+    console.log(i +' * '+ number +' = '+ i*number);
+  }
