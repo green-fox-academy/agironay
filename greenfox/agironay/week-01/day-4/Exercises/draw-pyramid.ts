39861d06830c41a016c5142fb50470e67e9dec34
Agi Ronay <agi.ronay93@gmail.com>
2019-06-28 23:55:20 +0200

    commit

---
+++ greenfox/agironay/week-01/day-4/Exercises/draw-pyramid.ts
@@ -0,0 +1,17 @@
+// Write a program that draws a
+// pyramid like this:
+//
+//    *
+//   ***
+//  *****
+// *******
+//
+// The pyramid should have as many lines as lineCount is
+
+let lineCount: number = 4;
+let star: string = ''
+
+for (let i: number = 0; i <= lineCount; i++) {
+    star +='*'
+    console.log(star)
+}
