119ce4f6ca58fffde21883a485bf988cb79079f7
Agi Ronay <agi.ronay93@gmail.com>
2019-07-01 17:13:30 +0200

    12th exc.

---
+++ greenfox/agironay/week-02/day-1/Exercises/diagonal-matrix.ts
@@ -0,0 +1,39 @@
+'use strict;'
+
+// -  Create (dynamically*) a two dimensional list
+//    with the following matrix**. Use a loop!
+//
+//    0 0 0 1
+//    0 0 1 0
+//    0 1 0 0
+//    1 0 0 0
+//
+// -  Print this two dimensional list to the console
+//
+// * size should depend on a variable
+// ** Relax, a matrix is just like an array
+
+'use strict';
+
+let matrix: number[][] = [
+  // [0 , 1 ,2 ,3,],
+];
+
+let size: number = 4;
+for (let index = 0; index < size; index++) {
+  let row: number[] = [];
+  for (let inner: number = 0; inner < size; inner++) {
+    if (size - inner - 1 === index) {
+      row.push(1);
+    } else {
+      row.push(0);
+    }
+  }
+  matrix.push(row);
+}
+
+matrix.forEach(function (row) {
+  console.log(row.join(" "));
+})
+
+//copied
