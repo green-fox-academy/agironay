f45dcec37dc17a043e5a4774566e206e62eb3112
Agi Ronay <agi.ronay93@gmail.com>
2019-07-01 14:13:55 +0200

    4th exc.

---
+++ greenfox/agironay/week-02/day-1/Exercises/sum-elements.ts
@@ -0,0 +1,10 @@
+// -  Create a variable named `r` with the following content: `[54, 23, 66, 12]`
+// -  Print the sum of the second and the third element
+
+'use strict;'
+
+let r: number [] = [54, 23, 66, 12];
+
+let sumofTwoThree: number[]; {
+    console.log(r[1]+r[2]);
+}
