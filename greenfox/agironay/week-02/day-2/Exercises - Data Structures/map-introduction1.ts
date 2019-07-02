855b7dab841af2e8e4865bd22d0180aad0736272
Agi Ronay <agi.ronay93@gmail.com>
2019-07-02 14:35:02 +0200

    4th exc. finished

--- greenfox/agironay/week-02/day-2/Exercises - Data Structures/map-introduction1.ts
+++ greenfox/agironay/week-02/day-2/Exercises - Data Structures/map-introduction1.ts
@@ -1,1 +1,30 @@
 'use strict;'
+
+let myMap = new Map ();
+
+console.log(myMap.size);
+
+myMap.set(97,'a');
+myMap.set(98,'b');
+myMap.set(99,'c');
+myMap.set(65,'A');
+myMap.set(66,'B');
+myMap.set(67,'C');
+
+//console.log(myMap);
+console.log(myMap.keys());
+console.log(myMap.values());
+
+myMap.set(68, 'D');
+
+console.log(myMap.size);
+
+console.log(myMap.get(99));
+
+myMap.delete(97);
+
+console.log(myMap.has(100));
+
+myMap.clear();
+
+console.log(myMap);
