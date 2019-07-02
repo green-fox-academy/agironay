06eb26a014716d4c9bc098a0b4652ab6fa614289
Agi Ronay <agi.ronay93@gmail.com>
2019-07-02 09:19:56 +0200

    morning

--- greenfox/agironay/week-02/day-1/Exercises-Arrays/reverse.ts
+++ greenfox/agironay/week-02/day-1/Exercises-Arrays/reverse.ts
@@ -15,5 +15,27 @@ let numList2 = [3, 4, 5, 6, 7];
 for (let i: number = 0; i < numList2.length; i++) {
     let newList = 0;
     newList = numList2[i];
-    console.log(newList);
+    //console.log(newList);
 }
+
+/*var itemsToIterate = myItems.slice(0).reverse();
+
+for (var i = 0, len = itemsToIterate.length; i < len; i++) {
+    var item = itemsToIterate[i];
+}*/
+
+
+
+/*function temporarySwap(array)
+{
+    var left = null;
+    var right = null;
+    var length = array.length;
+    for (left = 0, right = length - 1; left < right; left += 1, right -= 1)
+    {
+        var temporary = array[left];
+        array[left] = array[right];
+        array[right] = temporary;
+    }
+    return array;
+}*/
---
+++ greenfox/agironay/week-02/day-1/Exercises-Functions/sum.ts
@@ -0,0 +1,22 @@
+'use strict;'
+
+// Write a function called `sum` that returns the sum of numbers from zero to the given parameter
+
+function summ() {
+    let summ = 0;
+    for (let i = 0; i<upto.lenth;i++{
+        summ += upto[i];
+    }
+    return summ;
+}
+summ(2,4,5);
+
+/*function sum(){
+    var sum =0; 
+    for(var i=0;i<arguments.length;i++){
+       sum += arguments[i];
+    }
+     return sum;
+  }
+  sum(1,2); // returns 3
+  sum(1,2,3); // returns 6*/
