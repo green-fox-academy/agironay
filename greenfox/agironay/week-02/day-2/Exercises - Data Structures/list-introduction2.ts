34e3940fe9281ac5f003d25a526b2f8c413ab1fe
Agi Ronay <agi.ronay93@gmail.com>
2019-07-02 16:02:54 +0200

    done with lists pt.2

---
+++ greenfox/agironay/week-02/day-2/Exercises - Data Structures/list-introduction2.ts
@@ -0,0 +1,28 @@
+'use strict;'
+
+let ListA: string[] = ['Apple', 'Avocado', 'Blueberries', 'Durian', 'Lychee'];
+
+let ListB: string[] = ['Apple', 'Avocado', 'Blueberries', 'Durian', 'Lychee'];//copy the elements without referencing it 
+
+if (ListA.includes ('Durian')) {
+    console.log("YAY IT HAS IT");
+} else {
+    console.log("nope");
+}
+
+let deleteTheFruit = ListB.indexOf('Durian');
+if (deleteTheFruit > -1) {
+    ListB.splice(deleteTheFruit, 1);
+}
+
+ListA.push('Kiwifruit');
+
+console.log("ListA has:",ListA.length+" elements");
+console.log("ListB has:",ListB.length+" elements");
+
+console.log("The index number of Avocado is: "+ListA.indexOf('Avocado'));
+console.log("The index number of Durian in List B is: "+ListB.indexOf('Durian'));
+
+ListB.push('Passion Fruit', 'Pomelo');
+
+console.log("The third element in ListA is: ",ListA[2]);
