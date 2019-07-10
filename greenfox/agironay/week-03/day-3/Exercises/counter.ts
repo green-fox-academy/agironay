1a7e4caa9e3a292344f1f244e62cbe90d4b1a990
Agi Ronay <agi.ronay93@gmail.com>
2019-07-10 14:57:18 +0200

    exc. done

---
+++ greenfox/agironay/week-03/day-3/Exercises/counter.ts
@@ -0,0 +1,34 @@
+'use strict';
+
+class Counter {
+    integer: number;
+
+    constructor(integer: number = 0) {
+        this.integer = integer;
+    }
+
+    addNum(number): number{
+        return this.integer+=number;
+    }
+
+    add(): number {
+        return this.integer++;
+    }
+
+    get(): void {
+        return console.log('Currently our number is: ',this.integer);
+    }
+
+    reset(): number {
+        return this.integer=0;
+    }
+}
+
+let integer: Counter = new Counter();
+
+integer.addNum(5);
+integer.addNum(4);
+integer.add();
+integer.get();
+integer.reset();
+integer.get();
