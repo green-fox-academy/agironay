642a357acd959eadad5a55e1462c1287fd5edcb2
Agi Ronay <agi.ronay93@gmail.com>
2019-07-14 16:48:35 +0200

    not finished yet

---
+++ week-02/day-2/Exercises - Data Structures/productdatabase.ts
@@ -0,0 +1,22 @@
+'use strict';
+
+//How much is the fish?
+//What is the most expensive product?
+//What is the average price?
+//How many products' price is below 300?
+//Is there anything we can buy for exactly 125?
+//What is the cheapest product?
+
+
+let ListPrice:any = {
+    "Eggs":200,
+    "Milk":200,
+    "Fish":400,
+    "Apples":150,
+    "Bread":50,
+    "Chicken":550
+}
+
+console.log('The price of fish is: ',ListPrice['Fish']);
+
+//not finished yet
