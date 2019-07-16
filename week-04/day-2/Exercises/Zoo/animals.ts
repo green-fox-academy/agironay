78a35979b51ea130a1804786df58706d5a277ea1
Agi Ronay <agi.ronay93@gmail.com>
2019-07-16 16:09:17 +0200

    lazily finished the zoo

---
+++ week-04/day-2/Exercises/Zoo/animals.ts
@@ -0,0 +1,52 @@
+'use strict';
+
+abstract class Animals {
+    name: string;
+
+    constructor(name: string) {
+        this.name = name;
+    }
+
+    abstract getName();
+    abstract breed();
+}
+
+class Mammal extends Animals {
+    constructor(name: string) {
+        super(name)
+    }
+
+    getName() {
+        return this.name
+    }
+    breed() {
+        return 'ctrl c + ctrl v → and miniaturizes the bebeh';
+    }
+}
+
+class Reptile extends Animals {
+    constructor(name: string) {
+        super(name)
+    }
+
+    getName() {
+        return this.name;
+    }
+    breed() {
+        return 'laying eggs';
+    }
+}
+
+class Bird extends Animals {
+    constructor(name: string) {
+        super(name)
+    }
+    getName() {
+        return this.name;
+    }
+    breed() {
+        return 'laying eggs'
+    }
+}
+
+export {Mammal, Reptile, Bird};
