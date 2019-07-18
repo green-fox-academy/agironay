f7670e82e69206e337fe471b925f802e3ea92aaa
Agi Ronay <agi.ronay93@gmail.com>
2019-07-18 17:46:27 +0200

    not finished count letters

---
+++ week-04/day-4/Exercises/countletters.ts
@@ -0,0 +1,15 @@
+'use strict';
+
+function makeDict(takeThis: string) {
+    takeThis.map(function (item) {
+
+        for (let i: number = 0; i <= takeThis.length, i++) {
+
+        }
+
+        takeThis.length
+        return
+    }
+    )
+}
+
