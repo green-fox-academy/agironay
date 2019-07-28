cf3b955796b3df5e2a6c959fb756be00f216c61c
Agi Ronay <agi.ronay93@gmail.com>
2019-07-28 23:00:56 +0200

    pirate class done

---
+++ week-04/day-7 practice/pirate.ts
@@ -0,0 +1,44 @@
+'use strict';
+
+class Pirate {
+    name: string;
+    gold: number;
+    hp: number;
+    isCaptain: boolean;
+    hasWoodenLeg: boolean;
+
+    constructor(name: string, gold: number, hp: number = 10, isCaptain?: boolean, hasWoodenLeg?: boolean){
+        this.name = name;
+        this.gold = gold;
+        this.hp = hp;
+    }
+
+    work(): void{
+        if (this.isCaptain == true){
+            this.gold +=10;
+            this.hp -=5;
+        } else {
+            this.gold ++;
+            this.hp --;
+        }
+    }
+
+    party(): void{
+        if (this.isCaptain == true){
+            this.hp +=10;
+        } else {
+            this.hp ++;
+        }
+    }
+
+    toString (): string{
+        if (this.hasWoodenLeg == true){
+            return `Hello, I'm ${this.name}. I have a wooden leg and ${this.gold} golds.`
+        } else {
+            return `Hello, I'm ${this.name}. I still have my real legs and ${this.gold} golds.`
+        }
+    }
+
+}
+
+export default Pirate;
