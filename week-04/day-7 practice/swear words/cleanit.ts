3b0152803dcf3d854bc7786827b76e97ac9266e2
Agi Ronay <agi.ronay93@gmail.com>
2019-07-31 12:20:10 +0200

    it lists bad words excluding characters

--- week-04/day-7 practice/swear words/cleanit.ts
+++ week-04/day-7 practice/swear words/cleanit.ts
@@ -1,43 +1,29 @@
-'use strict';
+"use strict";
+
+import { readFileSync } from "fs";
 
 //Write a method which can read and parse a file containing not so family friendly text.
 //The method must remove all the given words from the file and return the amount of the removed words
 //Output = 17
 
-let badWords: string[] = ['fuck', 'bloody', 'cock', 'shit', 'fucker', 'fuckstick', 'asshole', 'dick', 'piss', 'cunt'];
+let badWords: string[] = ["fuck", "bloody", "cock", "shit", "fucker", "fuckstick", "asshole", "dick", "piss", "cunt"];
 
-const fs = require('fs');
+const fs = require("fs");
 
-let fileContent = fs.readFileSync('./badwords.txt', 'utf-8');
+let fileContent = fs.readFileSync("./badwords.txt", "utf-8");
 //console.log(fileContent);
 
-let badArr: string[] = []
-badArr.push(fileContent.split(' '))
-badArr = badArr.filter(e => e !== 'fuck');
-console.log(badArr)
+let lowerCaseWords: string[] = fileContent.toString().toLowerCase().split(" ");
+//console.log(lowerCaseWords)
 
-/*function makeArr() {
-    let badArr: string[] = []
-    badArr.push(fileContent.split(' '))
-    
-    
-    let index = badArr.indexOf(badWords[0]);
-    if (index !== -1) {
-        badArr.splice(index, 1);
+for (let i: number = 0; i < lowerCaseWords.length; i++) {
+  for (let j: number = 0; j < badWords.length; j++) {
+    let collectMatchingBadWords: string[] = [];
+    if (lowerCaseWords[i] !== badWords[j]) {
+      continue;
+    } else if (lowerCaseWords[i] == badWords[j]){
+      collectMatchingBadWords.push(lowerCaseWords[i]);
     }
-    console.log(badArr)
-    
-   /*  for (let i: number = 0; i < badArr.length; i++) {
-        if (badArr[i] == badWords[]) {
-            badArr.splice()
-        }
-    } 
-}*/
-
-//makeArr();
-
-
-function removeBadWords(fileName: string): number {
-
-    return
+    console.log(collectMatchingBadWords);
+  }
 }
