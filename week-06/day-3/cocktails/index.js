5b9709a095b4be82d37623eef06a47707960d48c
Agi Ronay <agi.ronay93@gmail.com>
2019-08-07 16:31:55 +0200

    cocktails listed in plain list

---
+++ week-06/day-3/cocktails/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const PORT = 3001;
+
+
+app.set('view engine', 'ejs')
+
+app.get('/cocktails', (req, res) => {
+    res.render('home', {
+        cocktails: [
+            { name: 'GIN FIZZ', price: 1520, contains: ['gin', 'sugar', 'lemon juice', 'soda'], isAlcoholic: true },
+            { name: 'BLOODY MARY', price: 1650, contains: ['vodka', 'tomato juice', 'spices'], isAlcoholic: true },
+            { name: 'SEX ON THE BEACH', price: 1850, contains: ['vodka', 'peach schnapps', 'orange juice', 'cranberry juice'], isAlcoholic: true },
+            { name: 'CUBA LIBRE', price: 1850, contains: ['rum', 'cola', 'lime juice'], isAlcoholic: true },
+            { name: 'MOJITO', price: 1850, contains: ['rum', 'sugar', 'lime juice', 'soda water'], isAlcoholic: true },
+            { name: 'LONG ISLAND ICE TEA', price: 2450, contains: ['vodka', 'rum', 'gin', 'tequila', 'triple sec', 'cola'], isAlcoholic: true },
+            { name: 'VIRGIN MOJITO', price: 990, contains: ['sugar', 'lime juice', 'soda water'], isAlcoholic: false },
+            { name: 'SAFE SEX ON THE BEACH', price: 990, contains: ['peach schnapps', 'orange juice', 'cranberry juice'], isAlcoholic: false },
+        ]
+    })
+}
+);
+
+app.get('/alcohols', (req, res)=> {
+    res.render('home', {
+        alcoholList = ['gin', 'vodka', 'rum', 'tequila']
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
