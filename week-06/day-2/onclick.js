58e4bd74aec89e736dd66e8b7b02911ef20cdf6e
Agi Ronay <agi.ronay93@gmail.com>
2019-08-13 23:18:36 +0200

    party still going

---
+++ week-06/day-2/onclick.js
@@ -0,0 +1,8 @@
+'use strict';
+//Turn the party on and off by clicking the button. (the whole page)
+
+let button = document.querySelector('button');
+
+button.onclick = () => {
+    alert('This just an alert to stop the party bros');
+}
