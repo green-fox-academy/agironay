fe92d822e9fa7fad8ad639aa1fb5c35a82a15ebe
Agi Ronay <agi.ronay93@gmail.com>
2019-08-14 14:00:02 +0200

    doubling okay

--- week-07/day-3/first/app.js
+++ week-07/day-3/first/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = 8080;
 
 
 app.use(express.static('assets'));
@@ -11,6 +11,16 @@ app.use(express.static('assets'));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
+
+app.get('/doubling/', (req, res) => {
+    if (req.query.input !== undefined) {
+        res.send({received: req.query.input,
+        result: req.query.input * 2})
+    } else {
+        res.send('error: Please provide an input!')
+    }
+    res.status(200);
+})
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
