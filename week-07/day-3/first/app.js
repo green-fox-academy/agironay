93a571dc0fcc4ad56cdbd7b0a992e0487ee2868a
Agi Ronay <agi.ronay93@gmail.com>
2019-08-14 15:52:28 +0200

    greeter done, gotta watch out for req, res order.. :)

--- week-07/day-3/first/app.js
+++ week-07/day-3/first/app.js
@@ -2,10 +2,12 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+//app.use('./assets',express.static('assets'));
 const PORT = 8080;
 
 
 app.use(express.static('assets'));
+app.use(express.json());
 
 
 app.get('/', (req, res) => {
@@ -14,13 +16,30 @@ app.get('/', (req, res) => {
 
 app.get('/doubling/', (req, res) => {
     if (req.query.input !== undefined) {
-        res.send({received: req.query.input,
-        result: req.query.input * 2})
+        res.send({
+            received: req.query.input,
+            result: req.query.input * 2
+        })
     } else {
         res.send('error: Please provide an input!')
     }
     res.status(200);
 })
+
+app.get('/greeter', (req, res) => {
+    if (req.query.name !== undefined && req.query.title !== undefined) {
+        res.send({ welcome_message: `Oh, hi there ${req.query.name}, my dear ${req.query.title}!` })
+    } else if (req.query.title == undefined && req.query.input == undefined) {
+        res.send({ error: `Please provide a name and a title!` })
+    } else if (req.query.name == undefined) {
+        res.send({ error: `Please provide a name!` })
+    } else {
+        res.send({ error: `Please provide a name and a title!` })
+    }
+    res.status(200);
+})
+
+
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
