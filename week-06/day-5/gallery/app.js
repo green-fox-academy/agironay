c28260c9ee7c150200aca4e48fcf8e30edc1f890
Agi Ronay <agi.ronay93@gmail.com>
2019-08-19 18:54:41 +0200

    gallery server, ejs, styles basic architecture done

---
+++ week-06/day-5/gallery/app.js
@@ -0,0 +1,50 @@
+'use strict';
+
+let myPix = [
+    {
+        url: '/static/pics/1.jpg',
+        title: 'Lofoten',
+        descriptiontext: 'Beautiful Lofoten',
+        first: 0
+    },
+    {
+        url: '/static/pics/2.jpg',
+        title: 'Senja',
+        descriptiontext: 'Senja in Norway'
+    },
+    {
+        url: '/static/pics/3.jpg',
+        title: 'Ylvis',
+        descriptiontext: 'I Kveld Med Yvlis - Norwegian talkshow'
+    },
+    {
+        url: '/static/pics/5.jpg',
+        title: 'Trolltunga',
+        descriptiontext: 'Hanging over Ringedalsvatnet'
+    },
+    {
+        url: '/static/pics/5.jpg',
+        title: 'Oslo',
+        descriptiontext: 'The city skyline'
+    },
+    {
+        url: '/static/pics/6.jpg',
+        title: 'Trollstigen',
+        descriptiontext: '55 kms of one of the most beautiful serpentine mountain roads.'
+    },
+    {
+        url: '/static/pics/7.jpg',
+        title: 'Preikestolen',
+        descriptiontext: 'Easy hike for a beautiful sight'
+    },
+    {
+        url: '/static/pics/8.jpg',
+        title: 'Kjeragbolten',
+        descriptiontext: 'Rock stuck between two rocks.'
+    },
+];
+
+/* let littlePictures = document.querySelectorAll('littlePictures');
+for (let i = 0; i < myPix.length; i++){
+    littlePictures[i]
+}; */
---
+++ week-06/day-5/gallery/server.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.set('view engine', 'ejs')
+app.use('/static', express.static('static'));
+
+app.get('/', (req, res) => {
+  res.render('home', {myPix: myPix });
+});
+
+let myPix = [
+  {url: '/static/pics/1.jpg', title: 'Lofoten', descriptiontext: 'Beautiful Lofoten', first: 0},
+  {url: '/static/pics/2.jpg', title: 'Senja', descriptiontext: 'Senja in Norway'},
+  {url: '/static/pics/3.jpg', title: 'Ylvis', descriptiontext: 'I Kveld Med Yvlis - Norwegian talkshow'},
+  {url: '/static/pics/5.jpg', title: 'Trolltunga', descriptiontext: 'Hanging over Ringedalsvatnet'},
+  {url: '/static/pics/5.jpg', title: 'Oslo', descriptiontext: 'The city skyline'},
+  {url: '/static/pics/6.jpg', title: 'Trollstigen', descriptiontext: '55 kms of one of the most beautiful serpentine mountain roads.'},
+  {url: '/static/pics/7.jpg', title: 'Preikestolen', descriptiontext: 'Easy hike for a beautiful sight'},
+  {url: '/static/pics/8.jpg', title: 'Kjeragbolten', descriptiontext: 'Rock stuck between two rocks.'},
+];
+
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
