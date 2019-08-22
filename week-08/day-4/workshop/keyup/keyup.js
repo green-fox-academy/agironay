8a0dce1fd547e27aa1b27de9dda6e28f4bb44cea
Agi Ronay <agi.ronay93@gmail.com>
2019-08-22 13:01:15 +0200

    w8 d4 keyup exc. done

---
+++ week-08/day-4/workshop/keyup/keyup.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/* 1) Subscibe to keyup events on the global window object
+2) Console log the event object and peek inside
+3) Display the last pressed key's code as "Last pressed key code is: __" */
+
+let displayCode = document.getElementsByTagName('h1')[0];
+
+document.addEventListener('keypress', logWhichKey);
+
+function logWhichKey(key) {
+    displayCode.textContent = `Last pressed key code is: ${key.code}`;
+}
