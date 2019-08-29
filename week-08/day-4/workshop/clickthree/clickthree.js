1a8df9a75a503df5267a8b2d1b3e5c25a362c00f
Agi Ronay <agi.ronay93@gmail.com>
2019-08-29 11:47:30 +0200

    w8 d4 click three not done

--- week-08/day-4/workshop/clickthree/clickthree.js
+++ week-08/day-4/workshop/clickthree/clickthree.js
@@ -2,20 +2,22 @@
 
 let button = document.querySelector('button');
 
+
+setTimeout(function () {
+  console.log('hi')
+}, 5000);
+
 let count = 0;
 function countClicks() {
+  if (count === 3) {
+    let newP = document.createElement('p');
+    document.body.appendChild(newP);
+    newP.innerHTML = '(5 seconds elapsed and) button was clicked 3 times'
+  } if (count < 3) {
     count++
-    if (count === 3) {
-        let newP = document.createElement('p');
-        document.body.appendChild(newP);
-        newP.innerHTML = '(5 seconds elapsed and) button was clicked 3 times'
-    }
+  }
 }
 
-/* document.captureEvents(countClicks); {
-    setTimeout(function () {
-        button.addEventListener('click', countClicks)
-    }, 5000);
-};
- */
+
+
 button.addEventListener('click', countClicks)
