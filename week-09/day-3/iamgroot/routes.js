cdcc7feef58ae669be4de627287352d82d85cf55
Agi Ronay <agi.ronay93@gmail.com>
2019-09-04 14:35:25 +0200

    w9d3 groot - yondu endpoint added

--- week-09/day-3/iamgroot/routes.js
+++ week-09/day-3/iamgroot/routes.js
@@ -13,4 +13,21 @@ app.get('/groot', (req, res) => {
   }
 });
 
+app.get('/yondu', (req, res) => {
+  if (req.query.distance === undefined && req.query.time === undefined) {
+    res.send('w r o n g, give distance and time')
+    res.sendStatus(400);
+  }else if (req.query.distance === undefined){
+    res.send('no, give me a distance pls')
+  } else if(req.query.time === undefined){
+    res.send('send me a time pls')
+  } else {
+    res.send({
+      distance: `${req.query.distance}`,
+      time: `${req.query.time}`,
+      speed: `${req.query.distance/req.query.time}`
+    });
+  }
+})
+
 module.exports = app;
