b86502c2fc8a98ec98e72384b82c258ff56db6a5
Agi Ronay <agi.ronay93@gmail.com>
2019-09-08 23:36:56 +0200

    w9d5 reddit frontend - tried to make it work on a server but css does not load, original setup is still ok

--- week-09/day-5/redditfront/assets/index.js
+++ week-09/day-5/redditfront/assets/index.js
@@ -7,19 +7,26 @@ fetch(url)
   .then(send);
 
 function send(data){
+  console.log(data.posts)
   for (let i = 0; i <data.posts.length; i++){
-    console.log(data.posts)
     let postsdiv = document.querySelector('.theposts');
     let div = document.createElement('div');
     let posttitle = document.createElement('h2');
     let posturl = document.createElement('h4');
-    let votes = document.createElement('p')
+    let votes = document.createElement('p');
+    let submitted = document.createElement('p');
     postsdiv.appendChild(div);
     div.appendChild(posttitle);
     div.appendChild(posturl);
     div.appendChild(votes);
+    div.appendChild(submitted)
     posttitle.innerHTML = data.posts[i].title;
     posturl.innerHTML = data.posts[i].url;
     votes.innerHTML = data.posts[i].score;
+    if (submitted.innerHTML = 'null'){
+      submitted.innerHTML = 'Submitted by: Anonymus';
+    } else {
+      submitted.innerHTML = 'Submitted by: '+data.posts[i].user;
+    }
   }
 }
---
+++ week-09/day-5/redditfront/server.js
@@ -0,0 +1,37 @@
+'use strict';
+
+//const app = require('../redditfront/app/app');
+const PORT = 3000;
+
+
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+
+//app.use(express.static(__dirname + '/views'));
+app.use(express.json());
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'reddit',
+});
+
+connection.connect(function (err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('Connection established');
+    }
+});
+
+app.get('/',(req, res)=>{
+    //res.send('hello')
+    res.sendFile('/views/index.html', { root: __dirname });
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
