b59b4be337dbb422402ecf63d186020bffb5b9e2
Agi Ronay <agi.ronay93@gmail.com>
2019-09-10 17:37:11 +0200

    w10 foxplayer - created some  enpoints, rendered sql data, working on add.post

--- week-10/Foxplayer/assets/working/main.js
+++ week-10/Foxplayer/assets/working/main.js
@@ -1,26 +1,28 @@
-
-
 let listPlaylists = document.querySelector('.list-playlist');
-
-
 let addPlaylist = document.querySelector('.addpic2');
-addPlaylist.addEventListener('click', function () {
-  console.log('playlist adder');
-  document.createElement
-});
 
-
-let removePlaylistButton = document.querySelectorAll('.delete1');
-removePlaylistButton.forEach(function (but) {
-  but.addEventListener('click', function () {
-    console.log('remove playlist');
+/* function createNewPlaylist(name) {
+  addPlaylist.addEventListener('click', function () {
+    let newDiv = document.createElement('div');
+    newDiv.setAttribute('class', 'playlist');
+    let newP = document.createElement('p');
+    listPlaylists.appendChild(newDiv);
+    newDiv.appendChild(newP);
+    newP.innerHTML = name;
+    let newImg = document.createElement('img');
+    newImg.src = 'imgs/delete.png';
+    newImg.setAttribute('class', 'delete1')
+    newDiv.appendChild(newImg);
   })
-});
+}; 
+createNewPlaylist('hi')
+*/
 
+//creating DOM elements
+let removePlaylistButton = document.querySelectorAll('.delete1');
 let prev = document.getElementById('prev');
 let next = document.getElementById('next');
 let play = document.getElementById('play');
-let pause = document.getElementById('pause');
 let shuffle = document.getElementById('shuffle');
 let volume = document.getElementById('volume');
 let timeElapsed = document.getElementById('time-elapsed');
@@ -28,32 +30,60 @@ let songSlider = document.getElementById('song-slider');
 let songLength = document.querySelectorAll('.song-length');
 let volumeSlider = document.getElementById('volume-slider');
 
+
+removePlaylistButton.forEach(function (but) {
+  but.addEventListener('click', function () {
+    console.log('remove playlist');
+  })
+});
+
+//play pause DOM events
 audio = new Audio();
-audio.src= '../music/02-fever_333-burn_it-60e7ec4c.mp3';
-function startMusic(){
-  audio.play();
-  songLength.innerHTML = audio.src.length;
-  console.log('play happened')
-}
-play.addEventListener('click', startMusic);
+audio.src = '../music/02-fever_333-burn_it-60e7ec4c.mp3';
 
-function pauseMusic(){
-  audio.pause();
-  console.log('pause happened')
-}
-pause.addEventListener('click', pauseMusic);
+let isPlaying = false;
+function playPause() {
+  if (isPlaying) {
+    audio.pause()
+    console.log('music paused')
+  } else {
+    audio.play();
+    console.log('music is playing')
+  }
+};
+audio.onplaying = function () {
+  isPlaying = true;
+};
+audio.onpause = function () {
+  isPlaying = false;
+};
 
-function muteMusic(){
+play.addEventListener('click', playPause);
+
+//space and m key events - play pause mute
+document.onkeydown = function (e) {
+  if (e.keyCode == 32) {
+    play.click();
+  } else if (e.keyCode == 32) {
+    pause.click();
+  } else if (e.keyCode == 77) {
+    audio.muted = true;
+  }
+};
+
+//mute audio
+function muteMusic() {
   audio.muted = true;
   console.log('muted')
 }
-
 volume.addEventListener('click', muteMusic);
 
-audio.onended = function() {
+audio.onended = function () {
   alert('Thanks for listening bros');
 };
 
-/* volume.addEventListener('click', function(){
-  console.log('hi')
-}); */
+/* let playlist = [
+  {url: '../music/02-fever_333-burn_it-60e7ec4c.mp3', title: 'Burn it', artist: 'Fever333', Length: '3:52'},
+  {url: './music/05 - Chlorine.mp3', title: 'Chlorine', artist: 'Twenty One Pilots', Length: '5:24'},
+  {url: '../music/Organoid_-_09_-_Purple_Drift.mp3', title: 'Purple Drift', artist: 'Organoid', Length: '3:28'},
+]; */
--- week-10/Foxplayer/server.js
+++ week-10/Foxplayer/server.js
@@ -2,14 +2,49 @@
 const express = require('express');
 const app = express();
 
-app.set('view engine','ejs')
+app.set('view engine', 'ejs')
 app.use(express.static('assets'));
 
 const PORT = 3000;
 
-app.get('/',(req,res)=>{
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'foxplayer'
+});
+
+connection.connect(function (err) {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log('Connection established');
+});
+
+app.get('/', (req, res) => {
     res.render('index');
-})
+});
+
+app.get('/listplaylists', (req, res)=>{
+    connection.query('SELECT id,title FROM playlists;', (err, rows)=>{
+        res.send(rows);
+    })
+});
+
+app.get('/playlists', (req, res)=>{
+    res.render('post')
+});
+
+app.post('/playlists',(req, res)=>{
+    let newPlaylist = req.body.title;
+    connection.query('INSERT INTO title VALUES (?)', newPlaylist.toString(), (err, rows)=>{
+        console.log(newPlaylist)
+    })
+    res.send(newPlaylist);
+});
 
 
 app.listen(PORT, () => {
