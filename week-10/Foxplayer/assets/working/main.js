cefef21c486e3d353cd890c26891e24358dcb112
Agi Ronay <agi.ronay93@gmail.com>
2019-09-10 11:10:59 +0200

    w10 foxplayer added music and play pause mute buttons that work

--- week-10/Foxplayer/assets/working/main.js
+++ week-10/Foxplayer/assets/working/main.js
@@ -1,15 +1,59 @@
-'use strict';
 
-let addPlaylist = document.querySelector('.addpic2');
+
 let listPlaylists = document.querySelector('.list-playlist');
 
+
+let addPlaylist = document.querySelector('.addpic2');
 addPlaylist.addEventListener('click', function () {
-    console.log('playlist adder');
-    document.createElement
+  console.log('playlist adder');
+  document.createElement
 });
 
-let removePlaylistButton = document.getElementById('delete1');
-removePlaylistButton.addEventListener('click', function () {
+
+let removePlaylistButton = document.querySelectorAll('.delete1');
+removePlaylistButton.forEach(function (but) {
+  but.addEventListener('click', function () {
     console.log('remove playlist');
-    document.createElement
+  })
 });
+
+let prev = document.getElementById('prev');
+let next = document.getElementById('next');
+let play = document.getElementById('play');
+let pause = document.getElementById('pause');
+let shuffle = document.getElementById('shuffle');
+let volume = document.getElementById('volume');
+let timeElapsed = document.getElementById('time-elapsed');
+let songSlider = document.getElementById('song-slider');
+let songLength = document.querySelectorAll('.song-length');
+let volumeSlider = document.getElementById('volume-slider');
+
+audio = new Audio();
+audio.src= '../music/02-fever_333-burn_it-60e7ec4c.mp3';
+function startMusic(){
+  audio.play();
+  songLength.innerHTML = audio.src.length;
+  console.log('play happened')
+}
+play.addEventListener('click', startMusic);
+
+function pauseMusic(){
+  audio.pause();
+  console.log('pause happened')
+}
+pause.addEventListener('click', pauseMusic);
+
+function muteMusic(){
+  audio.muted = true;
+  console.log('muted')
+}
+
+volume.addEventListener('click', muteMusic);
+
+audio.onended = function() {
+  alert('Thanks for listening bros');
+};
+
+/* volume.addEventListener('click', function(){
+  console.log('hi')
+}); */
