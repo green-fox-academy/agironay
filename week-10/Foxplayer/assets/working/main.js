5c9794e19e541ef47c42d90765afbe10afeb2ad1
Agi Ronay <agi.ronay93@gmail.com>
2019-09-11 15:04:56 +0200

    w10 foxplayer ejs post delete endpoints

--- week-10/Foxplayer/assets/working/main.js
+++ week-10/Foxplayer/assets/working/main.js
@@ -1,25 +1,11 @@
 let listPlaylists = document.querySelector('.list-playlist');
-let addPlaylist = document.querySelector('.addpic2');
+let addPlaylist = document.querySelectorAll('.addpic2');
+let removePlaylistButton = document.querySelectorAll('.delete1');
+let div = document.querySelectorAll('div');
+let listPlaylistDiv = document.querySelectorAll('playlist')
 
-/* function createNewPlaylist(name) {
-  addPlaylist.addEventListener('click', function () {
-    let newDiv = document.createElement('div');
-    newDiv.setAttribute('class', 'playlist');
-    let newP = document.createElement('p');
-    listPlaylists.appendChild(newDiv);
-    newDiv.appendChild(newP);
-    newP.innerHTML = name;
-    let newImg = document.createElement('img');
-    newImg.src = 'imgs/delete.png';
-    newImg.setAttribute('class', 'delete1')
-    newDiv.appendChild(newImg);
-  })
-}; 
-createNewPlaylist('hi')
-*/
 
 //creating DOM elements
-let removePlaylistButton = document.querySelectorAll('.delete1');
 let prev = document.getElementById('prev');
 let next = document.getElementById('next');
 let play = document.getElementById('play');
@@ -30,12 +16,13 @@ let songSlider = document.getElementById('song-slider');
 let songLength = document.querySelectorAll('.song-length');
 let volumeSlider = document.getElementById('volume-slider');
 
-
+//useless alert on trying to remove playlist item
 removePlaylistButton.forEach(function (but) {
   but.addEventListener('click', function () {
-    console.log('remove playlist');
+    alert('NO!! :@')
   })
 });
+
 
 //play pause DOM events
 audio = new Audio();
@@ -82,8 +69,9 @@ audio.onended = function () {
   alert('Thanks for listening bros');
 };
 
-/* let playlist = [
-  {url: '../music/02-fever_333-burn_it-60e7ec4c.mp3', title: 'Burn it', artist: 'Fever333', Length: '3:52'},
-  {url: './music/05 - Chlorine.mp3', title: 'Chlorine', artist: 'Twenty One Pilots', Length: '5:24'},
-  {url: '../music/Organoid_-_09_-_Purple_Drift.mp3', title: 'Purple Drift', artist: 'Organoid', Length: '3:28'},
-]; */
+/* let newImg = document.querySelectorAll('.delete1');*/
+
+
+/* const url = 'http://localhost:3000/tracks';
+fetch(url)
+  .then(response => response.json()) */
--- week-10/Foxplayer/server.js
+++ week-10/Foxplayer/server.js
@@ -4,10 +4,19 @@ const app = express();
 
 app.set('view engine', 'ejs')
 app.use(express.static('assets'));
+app.use(express.urlencoded());
 
 const PORT = 3000;
-
 const mysql = require('mysql');
+
+/* const fs = require('fs');
+const mm = require('musicmetadata');
+const readableStream = fs.createReadStream('sample.mp3');
+const parser = mm(readableStream, function (err, metadata) {
+  if (err) throw err;
+  readableStream.close();
+});
+ */
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -25,27 +34,54 @@ connection.connect(function (err) {
 });
 
 app.get('/', (req, res) => {
-    res.render('index');
+    connection.query('SELECT title FROM playlists;', (err, rows) => {
+        res.render('index', { rows });
+    });
 });
 
-app.get('/listplaylists', (req, res)=>{
-    connection.query('SELECT id,title FROM playlists;', (err, rows)=>{
+app.get('/playlists', (req, res) => {
+    res.render('post');
+});
+
+app.get('/listplaylists', (req, res) => {
+    connection.query('SELECT id,title FROM playlists;', (err, rows) => {
+        //res.render('index', {rows});
         res.send(rows);
     })
 });
 
-app.get('/playlists', (req, res)=>{
-    res.render('post')
-});
-
-app.post('/playlists',(req, res)=>{
-    let newPlaylist = req.body.title;
-    connection.query('INSERT INTO title VALUES (?)', newPlaylist.toString(), (err, rows)=>{
-        console.log(newPlaylist)
+app.get('/tracks',(req,res)=>{
+    connection.query('SELECT * FROM tracks;',(err,resp)=>{
+        res.send(resp);
     })
-    res.send(newPlaylist);
+})
+
+
+app.post('/playlists', (req, res) => {
+    let newPlaylist = req.body.title;
+    console.log(req.body)
+    connection.query('INSERT INTO playlists (title) VALUES (?);', [newPlaylist], (err, rows) => {
+        if (err) {
+            console.log(err);
+            res.sendStatus(500);
+        } else {
+            console.log('created new playlist called: ' + newPlaylist);
+            res.status(201);
+        }
+    })
 });
 
+app.delete('/playlists', (req, res) => {
+    let deletePlaylist = req.query.id;
+    connection.query('DELETE FROM playlists WHERE id =?', [deletePlaylist], (err, rows) => {
+        if (err) {
+            console.log(err);
+            res.sendStatus(500);
+        } else {
+            console.log(`deleted playlist called: `, +deletePlaylist);
+        }
+    })
+});
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
